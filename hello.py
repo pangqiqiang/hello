d206522547adf36ca6374688b9e09e2783770ab5
pangqiqiang <78733149@qq.com>
2018-06-02 13:01:05 +0800

    base example include bootstrap and moment

--- hello.py
+++ hello.py
@@ -1,2 +1,26 @@
 #!/usr/bin/env python3
 from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html",
+    current_time=datetime.utcnow())
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template("user.html", name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"),404
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
