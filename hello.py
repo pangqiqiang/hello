8f947ee89552486083d4a911878b01f25d16a1be
pangqiqiang <78733149@qq.com>
2018-06-02 18:02:26 +0800

    add wtform and flash

--- hello.py
+++ hello.py
@@ -4,15 +4,37 @@ from flask import render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask import session
+from flask import redirect
+from flask import url_for
+from flask import flash
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "zVrQqEAEJIUi10A9"
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route("/")
+
+class NameForm(Form):
+    name = StringField("What is your name?", validators=[Required()])
+    submit = SubmitField("Submit")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get("name")
+        if old_name is not None and old_name != form.name.data:
+            flash("Looks like you have changed your name!")
+        session["name"] = form.name.data
+        return redirect(url_for("index")) 
+
     return render_template("index.html",
-    current_time=datetime.utcnow())
+    current_time=datetime.utcnow(), form=form, name=session.get("name"))
 
 @app.route("/user/<name>")
 def user(name):
