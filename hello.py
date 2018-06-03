5095f09d9d9eecefb10629339f564686e4ac1ab5
pangqiqiang <78733149@qq.com>
2018-06-04 00:10:49 +0800

    create table add

--- hello.py
+++ hello.py
@@ -21,6 +21,7 @@ app.config["SECRET_KEY"] = "zVrQqEAEJIUi10A9"
 app.config["SQLALCHEMY_DATABASE_URI"] = \
 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -36,6 +37,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
     def __repr__(self):
         return '<ROLE %r>' % self.name
 
@@ -43,6 +45,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     def __repr__(self):
         return '<User %r>' % self.username
 
