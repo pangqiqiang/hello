f0ec9641a7e2cc2f139ec144ef156fb67aa64b06
pangqiqiang <78733149@qq.com>
2018-06-05 00:02:39 +0800

    add sqlalchemy to identify users

--- hello.py
+++ hello.py
@@ -37,7 +37,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
     def __repr__(self):
         return '<ROLE %r>' % self.name
 
@@ -51,17 +51,25 @@ class User(db.Model):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get("name")
-        if old_name is not None and old_name != form.name.data:
-            flash("Looks like you have changed your name!")
-        session["name"] = form.name.data
-        return redirect(url_for("index")) 
-
+        old_user = session.get("name")
+        if old_user and old_user != form.name.data:
+            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ""
+        return redirect(url_for("index"))
     return render_template("index.html",
-    current_time=datetime.utcnow(), form=form, name=session.get("name"))
+    form=form, name=session.get("name"),
+    known=session.get('known', False),
+    current_time=datetime.utcnow())
 
 @app.route("/user/<name>")
 def user(name):
